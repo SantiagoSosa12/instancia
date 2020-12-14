bb61b5653fe69f4dc78d3085c5ccfcd23556213a
SantiagoSosaReyes <santiago.sosa@uptc.edu.co>
2020-12-14 11:49:16 -0500

    Devolver imagenes

--- index.js
+++ index.js
@@ -2,13 +2,21 @@
 const express = require('express')
 const app = express()
 const port = 3000
+
 const path = require('path');
 const multer = require('multer');
 var imagenPrueba = 'imagenPrueba.png';
 var Jimp = require('jimp');
 var bodyParser = require('body-parser');
+
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+
+
+const FormData = require('form-data');
+const fs = require('fs');
+const axios = require('axios');
+const stream = fs.createReadStream('./subida/imagenPrueba.png');
 
 
 var imageCaption = 'La venganza nunca es buena mata el alma y la envenena';
@@ -45,10 +53,23 @@ app.post('/subir' , upload.single('file') , (req, res) => {
     return res.send(req.file);
 })
 
-app.post('/subir2' , upload.single('file') , (req, res) => {
-    console.log('Subida de un archivo NO SE PROCESA LA IMAGEN')
-    return res.send(req.file);
-})
+
+app.get('/devolver', (req, res) => {
+    reSendImage();
+    res.send('Intentando devolver imagen ...')
+});
+
+function reSendImage() {
+    var data = new FormData();
+    data.append("myImage", stream);
+    fromOtherServer = axios.post('http://192.168.0.11:3000/subir', data, data.getHeaders())
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
